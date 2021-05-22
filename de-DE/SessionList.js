5a25a226e7a99bd576de8dbe94d57c305616696f
flaymeariver <84620481+flaymeariver@users.noreply.github.com>
2021-05-22 15:22:29 +0200

    German Translation
    Added files with German translations of the given variables

---
+++ de-DE/SessionList.js
@@ -0,0 +1,6 @@
+export default {
+  'sessions': 'Sessions',
+  'addNewSession': 'Eine neue Session hinzufügen',
+  'noActiveSessions': 'Es wurden keine Sessions gefunden. Willst du eine neue erstellen?',
+  'noArchivedSessions': 'Du kannst Sessions für einen später Abruf archivieren!'
+}
---
+++ de-DE/Setup.js
@@ -0,0 +1,64 @@
+export default {
+  'step': 'Step {step}',
+  'Steps': {
+    'SessionName': 'Session Name',
+    'GameSelection': 'Spielauswahl',
+    'SetupEmulator': 'Emulator Konfiguration',
+    'ConnectionDetails': 'Verbindungsdetails'
+  },
+  'Welcome': {
+    'username': 'Nutzername'
+  },
+  'Details': {
+    'GameTypes': {
+      'solo': {
+        'description': 'Du spielst alleine ohne dich mit anderen Personen verbinden zu müssen',
+        'title': 'Einzelspieler'
+      },
+      'multi': {
+        'description': 'Du spielst mit einer oder mehreren Personen',
+        'title': 'Mehrspieler'
+      },
+      'multi_soullink': {
+        'description': 'Du spielst Soul Link mit einer oder mehreren Personen',
+        'title': 'Soul Link Mehrspieler'
+      },
+      'multi_vs': {
+        'description': 'Du spielst gegen eine oder mehrere Personen',
+        'title': 'VS Mehrspieler'
+      }
+    }
+  },
+  'SessionDetails': {
+    'SessionName': 'Session Name',
+    'Save': 'Die Konfiguration der Session fortsetzen',
+    'Validation': {
+      'SessionNameAndGameOptions': 'Du musst einen Session-Namen eingeben um fortzufahren'
+    }
+  },
+
+  'GameSelection': {
+    'DetectionTypes': {
+      'automatic': {
+        'description': 'Falls dein Spiel unterstützt wird, wird Pokélink die Daten live von deinem Emulator auslesen',
+        'title': 'Automatisches Team-Management'
+      },
+      'manual': {
+        'description': 'Du kannst dein Team manuell über ein simples Interface innerhalb der App bearbeiten',
+        'title': 'Manuelles Team-Management'
+      }
+    }
+  },
+  'ConnectionDetails': {
+    'ConnectionTypes': {
+      'host': {
+        'description': 'Du hast einen Port in deinem Router/Modem eröffnet oder hast eine andere Möglichkeit zur Verbindung eingerichtet.',
+        'title': 'Einen Server hosten'
+      },
+      'client': {
+        'description': 'Jemand anderes hat sein/ihr Netzwerk konfiguriert und ich werde mich zu deren Pokélink App verbinden.',
+        'title': 'Mit dem Server einer anderen Person verbinden'
+      }
+    }
+  }
+}
