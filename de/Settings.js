efea8a060df4f622e283d0cf738f37c9268bfcb1
Daniel Noel-Davies <NoelDavies@cybershade.org>
2021-06-01 22:49:32 +0100

    Add new files for nuzlocke tools and settings

--- de/Settings.js
+++ de/Settings.js
@@ -4,44 +4,90 @@ export default {
     'System': 'System-Einstellungen',
     'Window': 'Fenstereinstellungen',
     'Accessibility': 'Einstellungen zur Barrierefreiheit',
-    'GameAndEmulator': 'Spiel- & Emulator-Einstellungen'
+    'GameAndEmulator': 'Spiel- & Emulator-Einstellungen',
+    'ViewLogs': 'View Logs',
+    'Connections': 'Connections'
   },
   'Fields': {
     'SessionName': {
-      'Titles': 'Session-Name',
+      'Title': 'Session-Name',
       'Description': 'Bearbeite den Namen dieser Session'
     },
     'EmulatorLocation': {
-      'Titles': 'Ort des Emulators',
+      'Title': 'Ort des Emulators',
       'Description': 'Passe den Dateipfad deines Emulators an'
     },
     'RomLocation': {
-      'Titles': 'Ort der Rom',
+      'Title': 'Ort der Rom',
       'Description': 'Passe den Dateipfad deiner Rom an'
     },
     'RevealEggs': {
-      'Titles': 'Eier enthüllen',
+      'Title': 'Eier enthüllen',
       'Description': 'Ob der Inhalt eines Eis im Teambildschirm angezeigt werden soll'
     },
     'SendToTray': {
-      'Titles': 'Minimieren anstatt zu schließen',
+      'Title': 'Minimieren anstatt zu schließen',
       'Description': 'Anstatt das Programm zu schließen, wenn das X angeklickt wird, wird es minimiert'
     },
     'AllowErrorReporting': {
-      'Titles': 'Pokélink erlauben automatisch Fehlermeldungen zu senden',
+      'Title': 'Pokélink erlauben automatisch Fehlermeldungen zu senden',
       'Description': 'Das Deaktivieren dieser Option verhindert es, dass die Apps anonymisierte Daten an die Entwickler schickt, wenn Bugs auftreten'
     },
     'AutomaticallyBootEmulator': {
-      'Titles': 'Automatisch den Emulator starten, wenn eine Session wieder aufgenommen wird',
+      'Title': 'Automatisch den Emulator starten, wenn eine Session wieder aufgenommen wird',
       'Description': 'Deaktiviere diese Option, falls du den Emulator lieber selber starten willst'
     },
     'EnableDyslexiaFont': {
-      'Titles': 'Schriftart für Legastheniker aktivieren',
+      'Title': 'Schriftart für Legastheniker aktivieren',
       'Description': 'Schriftart aktivieren (nur in der App), die Legasthenikern das Lesen erleichtert'
     },
     'EnableDarkMode': {
-      'Titles': 'Aktivierung dunkler Modus',
+      'Title': 'Aktivierung dunkler Modus',
       'Description': 'Tu deinen Augen einen Gefallen mit dem Aktivieren des dunklen Themes'
+    },
+    'TwitchBot': {
+      'Title': 'Twitch Bot'
+    },
+    'TwitchBotLogin': {
+      'Title': 'Twitch Bot Login',
+      'Description': 'Connect your Twitch account to enable Twitch-integrated features'
+    },
+    'TwitchBotStatus': {
+      'Title': 'Twitch Bot Status',
+      'Description': 'Stop & Start the bot'
+    },
+    'TwitchBotCommands': {
+      'Title': 'Customise the Bot Command names',
+      'Description': 'Change the commands that the this Twitch Bot will respond to'
+    }
+  },
+  'Actions': {
+    'Connections': {
+      'TwitchAccount': {
+        'Login': 'Login With your twitch account',
+        'Logout': 'Disconnect your twitch account',
+        'LoggedInAs': 'Logged in as {twitchName}'
+      },
+      'TwitchBotStatus': {
+        'Start': 'Start Bot',
+        'Stop': 'Stop Bot'
+      }
+    }
+  },
+  'Connections': {
+    'TwitchBot': {
+      'Commands': {
+        'listCommands': 'The command used to list available commands',
+        'team': 'The command that will list your team',
+        'stats': 'The command that will show the stats of a specific pokemon in your party',
+        'evs': 'The command that will show the evs of a specific pokemon in your party',
+        'ivs': 'The command that will show the ivs of a specific pokemon in your party',
+        'ability': 'The command that will show the ability of a specific pokemon in your party',
+        'nature': 'The command that will show the nature of a specific pokemon in your party',
+        'faster': 'The command that will show which pokemon is faster between your pokemon and a specified Pokemon species',
+        'moves': 'The command that will show the current moves of a specific pokemon in your party',
+        'deaths': 'The command that will show the deaths that pokelink has tracked for this session'
+      }
     }
   }
 }
