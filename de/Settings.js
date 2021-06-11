e4a17a444000161359177356b74c44ffc57de026
flaymeariver <84620481+flaymeariver@users.noreply.github.com>
2021-06-11 11:12:10 +0200

    second conflict resolve
    I'm blind

--- de/Settings.js
+++ de/Settings.js
@@ -4,45 +4,45 @@ export default {
     'System': 'System-Einstellungen',
     'Window': 'Fenstereinstellungen',
     'Accessibility': 'Einstellungen zur Barrierefreiheit',
-    'GameAndEmulator': 'Spiel- & Emulator-Einstellungen'
+    'GameAndEmulator': 'Spiel- & Emulator-Einstellungen',
     'ViewLogs': 'View Logs',
     'Connections': 'Connections'
   },
   'Fields': {
     'SessionName': {
-      'Titles': 'Session-Name',
+      'Title': 'Session-Name',
       'Description': 'Bearbeite den Namen dieser Session'
     },
     'EmulatorLocation': {
-      'Titles': 'Ort des Emulators',
+      'Title': 'Ort des Emulators',
       'Description': 'Passe den Dateipfad deines Emulators an'
     },
     'RomLocation': {
-      'Titles': 'Ort der Rom',
+      'Title': 'Ort der Rom',
       'Description': 'Passe den Dateipfad deiner Rom an'
     },
     'RevealEggs': {
-      'Titles': 'Eier enthüllen',
+      'Title': 'Eier enthüllen',
       'Description': 'Ob der Inhalt eines Eis im Teambildschirm angezeigt werden soll'
     },
     'SendToTray': {
-      'Titles': 'Minimieren anstatt zu schließen',
+      'Title': 'Minimieren anstatt zu schließen',
       'Description': 'Anstatt das Programm zu schließen, wenn das X angeklickt wird, wird es minimiert'
     },
     'AllowErrorReporting': {
-      'Titles': 'Pokélink erlauben automatisch Fehlermeldungen zu senden',
+      'Title': 'Pokélink erlauben automatisch Fehlermeldungen zu senden',
       'Description': 'Das Deaktivieren dieser Option verhindert es, dass die Apps anonymisierte Daten an die Entwickler schickt, wenn Bugs auftreten'
     },
     'AutomaticallyBootEmulator': {
-      'Titles': 'Automatisch den Emulator starten, wenn eine Session wieder aufgenommen wird',
+      'Title': 'Automatisch den Emulator starten, wenn eine Session wieder aufgenommen wird',
       'Description': 'Deaktiviere diese Option, falls du den Emulator lieber selber starten willst'
     },
     'EnableDyslexiaFont': {
-      'Titles': 'Schriftart für Legastheniker aktivieren',
+      'Title': 'Schriftart für Legastheniker aktivieren',
       'Description': 'Schriftart aktivieren (nur in der App), die Legasthenikern das Lesen erleichtert'
     },
     'EnableDarkMode': {
-      'Titles': 'Aktivierung dunkler Modus',
+      'Title': 'Aktivierung dunkler Modus',
       'Description': 'Tu deinen Augen einen Gefallen mit dem Aktivieren des dunklen Themes'
     },
     'TwitchBot': {
