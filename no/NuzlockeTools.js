71b454a149b8f882e885ded728f299410584025d
gorutotwitch <63480218+gorutotwitch@users.noreply.github.com>
2021-06-27 23:06:39 +0200

    Update NuzlockeTools.js To Norwegian

--- no/NuzlockeTools.js
+++ no/NuzlockeTools.js
@@ -1,32 +1,32 @@
 export default {
   'Sections': {
     'DeathTracking': {
-      'Title': 'Death Tracker',
-      'DeathExplanation': 'If a Pokémon is revived and hits 0 HP again, it will be tracked as 2 deaths. Clicking the cross will "forget" the death forever.',
-      'NoResults': 'No deaths match your search criteria',
-      'NoDeaths': 'You\'ve not had any deaths in this session',
-      'TimeOfDeath': 'Time of death',
-      'ResetAllDeaths': 'Reset all deaths'
+      'Title': 'Dødsregister',
+      'DeathExplanation': 'Hvis en Pokémon blir gjenopplivet og går ned til 0 HP igjen, så vil det bli registrert som 2 individuelle døds tidspunkt. Å trykke på + tegnet vil fjerne registeret for alltid.',
+      'NoResults': 'Det er null resultater i dødsregisteret for ditt søk.',
+      'NoDeaths': 'Du har ikke ingen dødstilfeller i dette spillet... så langt.',
+      'TimeOfDeath': 'Registrert klokkeslett for døden.',
+      'ResetAllDeaths': 'Resett alle døde'
     },
     'ShowdownExport': {
-      'Title': 'Showdown Export',
-      'ContentTitle': 'Export your team to showdown',
-      'ProcessDescription': 'This can be used to import your team into showdown and even various online damage calculators.'
+      'Title': 'Showdown Eksport',
+      'ContentTitle': 'Eksporter ditt lag til Pokémon Showdown.',
+      'ProcessDescription': 'Dette kan bli brukt for å importere laget ditt til Pokémon Showdown samt andre skade kalkulatorer som tillater det.'
     },
     'RouteTracking': {
-      'Title': 'Route Tracker',
-      'SupportInfo': 'Routes now track automatically in Gens 3,4,5,6 &amp; 7 and also on manual sessions using the "Location Met" field.',
-      'OnlyPokemonInYourPartyAreTracked': 'Note: The pokemon must be in your party for it to automatically register.',
-      'YouCanManuallyTrackThemToo': 'You can also manually track the routes by clicking a button below the route name.',
+      'Title': 'Route Loggføring',
+      'SupportInfo': 'Pokemon fanget på unike Route i spill fra Generasjon 3,4,5,6 & 7 blir nå automatisk loggført, også i Manuell Spill under "Møtt Hvor" feltet.',
+      'OnlyPokemonInYourPartyAreTracked': 'PS: Pokémonen må være på laget ditt for å bli automatisk loggført.',
+      'YouCanManuallyTrackThemToo': 'Du kan også manuelt loggføre unike Route ved å klikke på knappen under Route navnet.',
       'States': {
-        'Caught': 'Caught',
-        'Gifted': 'Gifted',
-        'Traded': 'Traded',
-        'Escaped': 'Escaped',
-        'Killed': 'Killed'
+        'Caught': 'Fanget',
+        'Gifted': 'Gave',
+        'Traded': 'Byttet',
+        'Escaped': 'Rømte',
+        'Killed': 'Drept'
       },
-      'SearchForARoute': 'Search for a route',
-      'ResetAllRoutes': 'Reset status of all routes'
+      'SearchForARoute': 'Søk etter unik Route',
+      'ResetAllRoutes': 'Resett alle Routes'
     }
   }
 }
