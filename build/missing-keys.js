e0ab18f1d255a94a37586e02e843ba08094678d6
Jez <daniel@noeldavi.es>
2025-05-16 16:31:18 +0100

    Add missing keys git pre commit hook

--- build/missing-keys.js
+++ build/missing-keys.js
@@ -1,82 +1,140 @@
 const fs = require( 'fs' );
-const { flushCompileCache } = require( 'module' );
 const path = require( 'path' );
 
 // Function to read JSON from file
 const readJson = ( filepath ) => {
-  const langPath = path.join( filepath );
-  const langFileContent = fs.readFileSync( langPath, 'utf8' );
+  try {
+    const data = fs.readFileSync( filepath, 'utf8' );
 
-  // parse the JSON content into a JavaScript object
-  const translations = JSON.parse( langFileContent );
+    // Trim whitespace from the beginning and end of the JSON data
+    const trimmedData = data.trim();
 
-  return translations
+    // Use a more robust JSON parser (parse-json) for better handling of formatting issues
+    return JSON.parse( trimmedData );
+  } catch ( error ) {
+    console.error( `Error reading file ${filepath}:`, error.message );
+    throw error;
+  }
 };
 
-
-// Function to build a nested array from .lang files
+// Function to build a nested array from the JSON content of all .lang files in a directory
 const buildNestedArray = ( directory ) => {
   const files = fs.readdirSync( directory ).filter( file => file.endsWith( '.lang' ) );
   const nestedArray = {};
-
   files.forEach( file => {
-
     const filePath = path.join( directory, file );
-    const fileKey = path.basename( file, '.lang' );
-
+    const fileKey = path.basename( file, '.lang' ); // Remove the .lang extension
     try {
-      console.log( `Reading file: ${filePath}` );
+      // console.log( `Reading file: ${filePath}` );
       nestedArray[ fileKey ] = readJson( filePath );
     } catch ( error ) {
       console.error( `Error reading file ${filePath}:`, error );
     }
   } );
-
-
   return nestedArray;
 };
 
-const getLanguageDirectoriesWithNeoDir = ( languages ) => {
-  const baseDir = path.join( __dirname, '../' );
-  const languageFolders = [];
-
-  languages.forEach( lang => {
-    const langDir = path.join( baseDir, lang, 'neo' );
-    if ( fs.existsSync( langDir ) && fs.lstatSync( langDir ).isDirectory() ) {
-      languageFolders.push( lang );
+const addMissingKeysToDestinationObject = ( source, destination ) => {
+  for ( const key in source ) {
+    if ( source.hasOwnProperty( key ) ) {
+      if ( !destination.hasOwnProperty( key ) ) {
+        destination[ key ] = source[ key ];
+      } else if ( typeof source[ key ] === 'object' && typeof destination[ key ] === 'object' ) {
+        addMissingKeysToDestinationObject( source[ key ], destination[ key ] );
+      }
     }
-  } );
-  return languageFolders;
+  }
 }
 
-const buildLanguagesFromTranslationDirectories = ( languages ) => {
+const getLanguageDirectoriesWithNeoDir = () => {
   const baseDir = path.join( __dirname, '../' );
-  const builtLanguages = {};
-
-  languages.forEach( lang => {
-    const langDir = path.join( baseDir, lang, 'neo' );
-    if ( fs.existsSync( langDir ) && fs.lstatSync( langDir ).isDirectory() ) {
-      builtLanguages[ lang ] = buildNestedArray( langDir );
-    }
+  const languageFolders = fs.readdirSync( baseDir ).filter( dir => {
+    const langDir = path.join( baseDir, dir, 'neo' );
+    return fs.existsSync( langDir ) && fs.lstatSync( langDir ).isDirectory();
   } );
-  return builtLanguages;
+  return languageFolders;
 };
 
-// Build the nested array from en-GB/neo directory
+const calculateNewlyAddedKeys = ( originLanguage, targetLanguage ) => {
+  const newKeys = {};
+
+  for ( const key in originLanguage ) {
+    if ( !targetLanguage.hasOwnProperty( key ) ) {
+      newKeys[ key ] = originLanguage[ key ];
+    } else if ( typeof originLanguage[ key ] === 'object' && typeof targetLanguage[ key ] === 'object' ) {
+      const nestedNewKeys = calculateNewlyAddedKeys( targetLanguage[ key ], originLanguage[ key ] );
+      if ( Object.keys( nestedNewKeys ).length > 0 ) {
+        newKeys[ key ] = nestedNewKeys;
+      }
+    }
+  }
+
+  return newKeys;
+};
+
+const formatKeyPaths = ( obj, prefix = '' ) => {
+  let result = {};
+
+  for ( const key in obj ) {
+    const newKey = prefix ? `${prefix}.${key}` : key;
+    if ( typeof obj[ key ] === 'object' && !Array.isArray( obj[ key ] ) && obj[ key ] !== null ) {
+      Object.assign( result, formatKeyPaths( obj[ key ], newKey ) );
+    } else {
+      result[ newKey ] = obj[ key ];
+    }
+  }
+
+  return result;
+};
+
+const processLanguageDirectory = ( fromOriginLanguage, language ) => {
+  const currentTranslationLanguage = buildNestedArray( path.join( __dirname, '../' + language + '/neo' ) );
+
+  // Calculate and log newly added keys before adding missing keys
+  const newKeys = calculateNewlyAddedKeys( fromOriginLanguage, currentTranslationLanguage );
+  if ( Object.keys( newKeys ).length > 0 ) {
+    // console.log( '\n\nNewly added keys in' + language + ':\n' );
+    const formattedKeys = formatKeyPaths( newKeys );
+    Object.keys( formattedKeys ).sort().forEach( key => {
+      console.log( `${language}: ${key}` );
+    } );
+  }
+
+  addMissingKeysToDestinationObject( fromOriginLanguage, currentTranslationLanguage );
+  writeNeoTranslationsToLanguageDirectoryStructure( currentTranslationLanguage, language );
+};
+
+const writeNeoTranslationsToLanguageDirectoryStructure = ( translations, language ) => {
+  const languageNeoDir = path.join( __dirname, '../', language, 'neo' );
+
+  // Create the base directory if it doesn't exist
+  if ( !fs.existsSync( languageNeoDir ) ) {
+    fs.mkdirSync( languageNeoDir, { recursive: true } );
+  }
+
+  // Write each translation to its respective .lang file
+  for ( const key in translations ) {
+    if ( translations.hasOwnProperty( key ) ) {
+      const filePath = path.join( languageNeoDir, `${key}.lang` );
+      try {
+        let langfileContent = {
+          ...translations[ key ],
+          code: language,
+        };
+        fs.writeFileSync( filePath, JSON.stringify( langfileContent, null, 2 ) );
+        // console.log( `Written ${filePath}` );
+      } catch ( error ) {
+        console.error( `Error writing file ${filePath}:`, error );
+      }
+    }
+  }
+}
+
 const enGBTranslations = buildNestedArray( path.join( __dirname, '../en-GB/neo' ) );
+const languageFolders = getLanguageDirectoriesWithNeoDir();
 
-let definedLanguages = [
-  'de', 'fr-BE', 'it', 'nl-BE', 'no'
-];
-
-const languageFolders = getLanguageDirectoriesWithNeoDir( definedLanguages );
-
-let builtLanguages = buildLanguagesFromTranslationDirectories( languageFolders );
-
-console.info( builtLanguages );
-
-console.table( 'Translations', buildNestedArray );
-console.info( builtLanguages );
-
-
-console.info( 'All translations checked.' );
+languageFolders.forEach( language => {
+  if ( language !== 'en-GB' ) {
+    processLanguageDirectory( enGBTranslations, language );
+  }
+} );
