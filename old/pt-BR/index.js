1b85487014c052f9ad7dbbf6138780186896987b
NimbusFox <nimbusfox69@outlook.com>
2025-07-15 12:14:40 +0100

    Updating new languages to 0.7

---
+++ old/pt-BR/index.js
@@ -0,0 +1,23 @@
+import { readdirSync } from 'fs'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const getFiles = source =>
+    readdirSync(source, {withFileTypes: true})
+        .filter(fileEnt => fileEnt.isFile() && fileEnt.name.endsWith('.js') && fileEnt.name !== 'index.js')
+        .map(fileEnt => fileEnt.name)
+const modules = {}
+
+for (const file of getFiles(__dirname)) {
+  modules[file.replace(/(\.\/|\.js)/g, '')] = (await import(`${__dirname}/${file}`)).default
+}
+
+export default {
+  label: 'Portuguese (BR)',
+  code: 'pt-BR',
+  flag: 'BR',
+  translations: {
+    ...modules
+  }
+}
